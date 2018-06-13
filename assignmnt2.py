27f929eb49720d79184ad09b06b2009352459c78
varchla <vthakur1207@gmail.com>
2018-06-13 15:21:58 +0530

    assignmnt2 done

---
+++ assignmnt2.py
@@ -0,0 +1,31 @@
+#ques 1
+
+print("hello")
+
+#ques 2
+
+print("acad"+"view")
+
+#ques 3
+
+x=input("enter value for x")
+y=input("enter value for y")
+z=input("enter value for z")
+print(x,y,z)
+
+#ques 4
+
+print('let\'s get started')
+
+#ques 5
+
+s="acadview"
+course="python"
+fees=5000
+print('welcome to %s and the cousre is %s and the fees is %d'%(s,course,fees))
+
+#ques 6
+
+name="tony stark"
+salary=1000000
+print('the name is %s and the salary is %d'%(name,salary))
